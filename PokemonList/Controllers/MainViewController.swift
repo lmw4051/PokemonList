5b0405106a651c9d75645e176f598141ad39f879
David <dvdlee41@gmail.com>
2020-11-17 21:25:00 +0800

    . Adding MasterViewController . Set up MasterViewController and NavigationController in SceneDelegate

---
+++ PokemonList/Controllers/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  PokemonList
+//
+//  Created by David on 2020/11/17.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
